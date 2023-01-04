d46b2b4d344a7bbcaa34d49f449015c026e30a5d
Aditya Fitriadi <aditya.fitriadi99@gmail.com>
2023-01-04 15:25:57 +0700

    delete book

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ const {
   addBookHandler,
   getAllBooksHandler,
   getBookByIdHandler,
-  editBookByIdHandler
+  editBookByIdHandler,
+  deleteBookByIdHandler
 } = require('./handler')
 
 const routes = [
@@ -25,6 +26,11 @@ const routes = [
     method: 'PUT',
     path: '/books/{id}',
     handler: editBookByIdHandler
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler
   }
 ]
 
